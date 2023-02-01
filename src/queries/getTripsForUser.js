670f35b71a4cf046e1e057ba25c1f25d70149c3b
Fabian Simon <fabian.simon98@gmail.com>
2023-02-01 16:12:58 +0000

    lol

--- src/queries/getTripsForUser.js
+++ src/queries/getTripsForUser.js
@@ -1,5 +1,6 @@
 import { ApolloError, AuthenticationError } from "apollo-server-express";
 import Image from "../models/Image.model.js";
+import Task from "../models/Task.model.js";
 import Trip from "../models/Trip.model.js";
 import User from "../models/User.model.js";
 
@@ -10,26 +11,63 @@ export const getTripsForUser = async (_, __, { userId }) => {
   try {
     const userData = await User.findById(userId);
 
-    const tripData = await Trip.find({
+    const trips = await Trip.find({
       _id: {
         $in: userData.trips,
       },
     });
 
-    const trips = await Promise.all(
-      tripData.map(async (trip) => {
+    const now = Date.now() / 1000;
+    let recapTimestamp = new Date();
+    recapTimestamp.setFullYear(recapTimestamp.getFullYear() - 1);
+    recapTimestamp = Date.parse(recapTimestamp) / 1000;
+
+    const tripData = await Promise.all(
+      trips.map(async (trip) => {
+        const { dateRange } = trip;
         let _images = await Image.find({
           _id: {
             $in: trip.images,
           },
         });
+        let _activeMembers = await User.find({
+          _id: {
+            $in: trip.activeMembers,
+          },
+        });
+
+        // const isActive =
+        //   dateRange.startDate < now && dateRange.endDate > now;
+        // const isRewind =
+        //   dateRange.startDate < recapTimestamp && dateRange.endDate < now;
+
+        const isUpcoming = ((dateRange.startDate - now) / 86400).toFixed(0) < 7;
+
+        if (isUpcoming) {
+          const mutualTasks = await Task.find({
+            _id: {
+              $in: trip.mutualTasks,
+            },
+          });
+
+          const privateTasks = await Task.find({
+            _id: {
+              $in: trip.privateTasks,
+            },
+          })
+            .where("creatorId")
+            .equals(userId);
+
+          trip.openTasks = [...mutualTasks, ...privateTasks];
+        }
 
         trip.images = _images;
+        trip.activeMembers = _activeMembers;
         return trip;
       })
     );
 
-    return trips;
+    return tripData;
   } catch (error) {
     throw new ApolloError(error);
   }
