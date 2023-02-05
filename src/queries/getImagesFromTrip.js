dc66b42dcbed14c953770e88f05b01175197807b
Fabian Simon <fabian.simon98@gmail.com>
2023-02-05 16:54:33 +0000

    calculate Free images

--- src/queries/getImagesFromTrip.js
+++ src/queries/getImagesFromTrip.js
@@ -2,6 +2,11 @@ import { ApolloError, AuthenticationError } from "apollo-server-express";
 import Image from "../models/Image.model.js";
 import Trip from "../models/Trip.model.js";
 import User from "../models/User.model.js";
+import {
+  daySegments,
+  DAILY_IMAGE_CAP,
+  IMAGES_PER_CHUNK,
+} from "../utils/PushNotificationService.js";
 
 export const getImagesFromTrip = async (_, { tripId }, { userId }) => {
   if (!userId) {
@@ -9,7 +14,11 @@ export const getImagesFromTrip = async (_, { tripId }, { userId }) => {
   }
 
   try {
-    const { images: tripImages } = await Trip.findById(tripId);
+    const {
+      images: tripImages,
+      dateRange,
+      activeMembers,
+    } = await Trip.findById(tripId);
 
     let images = await Image.find({
       _id: {
@@ -25,6 +34,22 @@ export const getImagesFromTrip = async (_, { tripId }, { userId }) => {
       },
     });
 
+    const now = new Date() / 1000;
+    const tripLength = Math.abs((dateRange.startDate - now) / 86400).toFixed(0);
+
+    let totalImages = tripLength * DAILY_IMAGE_CAP;
+
+    const currHour = new Date().getHours();
+    for (var i = 0; i < daySegments.length; i++) {
+      const { end } = daySegments[i];
+      if (end > currHour) break;
+      else {
+        totalImages += parseInt(IMAGES_PER_CHUNK);
+      }
+    }
+
+    const imagesToAdd = (totalImages / activeMembers.length).toFixed(0);
+
     images = images.map((image) => {
       return {
         ...image._doc,
@@ -32,7 +57,10 @@ export const getImagesFromTrip = async (_, { tripId }, { userId }) => {
       };
     });
 
-    return images;
+    return {
+      images,
+      userFreeImages: imagesToAdd,
+    };
   } catch (error) {
     throw new ApolloError(error);
   }
