dcb3434f6797f6c38242f55d1eb4a56735f6093e
Fabian Simon <fabian.simon98@gmail.com>
2023-05-13 14:45:22 +0100

    added appleSignup

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -8,6 +8,9 @@ const UserSchema = new mongoose.Schema(
     googleId: {
       type: String
     },
+    appleId: {
+      type: String
+    },
     email: {
       type: String,
       required: true,
